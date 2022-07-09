1eda8b35ff41b5ee941adcf33046bf1dd3dbdbf3
ShinYoung-Kim <lukey0515@naver.com>
2022-07-09 09:57:24 +0900

    setting panel 분리

---
+++ SEProject/src/se/tetris/setting/SettingInterface.java
@@ -0,0 +1,18 @@
+package se.tetris.setting;
+
+import se.tetris.data.DBCalls;
+
+import javax.swing.*;
+import java.awt.*;
+
+public interface SettingInterface<T> {
+
+    JLabel Title = null;
+    JRadioButton selectOne = null;
+    JRadioButton selectTwo = null;
+    JRadioButton selectThree = null;
+
+    abstract void update(T type);
+    abstract void reload();
+    abstract void changeEachSettingSize();
+}
